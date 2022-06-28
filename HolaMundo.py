f5e7efcc111a2c70d895e785b259bb8783d0314c
osvaldoibarra <ohi1498@gmail.com>
2022-06-28 15:10:05 -0500

    update

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,6 +1,4 @@
 # Hola mundo en Python
 
-print "Hola mundo!"
-print "Me esta gustando Python" 
-nombre = raw_input("Dime como te llamas: ")
-print "Encantado de saludarte ", nombre
+print ("Hola mundo!")
+print ("Me esta gustando Python")
